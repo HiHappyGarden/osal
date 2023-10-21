9b492800c21e5ec4c93ab6bed7ab5b27de3ccbab
Antonio Salsi <passy.linux@zresa.it>
2023-10-21 21:12:06 +0200

    0.6.0

--- inc/osal/config.h
+++ inc/osal/config.h
@@ -2,9 +2,9 @@
 #define OSAL_CONFIG_H
 
 #define OSAL_NAME "OSAL"
-#define OSAL_VER "0.5.1"
+#define OSAL_VER "0.6.0"
 #define OSAL_VER_MAJOR 0
-#define OSAL_VER_MINOR 5
-#define OSAL_VER_PATCH 1
+#define OSAL_VER_MINOR 6
+#define OSAL_VER_PATCH 0
 
 #endif // OSAL_CONFIG_H
