7e9b3efac0a19b357c81027c8cc1bcad88162ea4
Antonio Salsi <passy.linux@zresa.it>
2023-11-12 22:24:13 +0100

    0.7.0 (braking change)

--- inc/osal/config.h
+++ inc/osal/config.h
@@ -2,9 +2,9 @@
 #define OSAL_CONFIG_H
 
 #define OSAL_NAME "OSAL"
-#define OSAL_VER "0.6.0"
+#define OSAL_VER "0.7.0"
 #define OSAL_VER_MAJOR 0
-#define OSAL_VER_MINOR 6
+#define OSAL_VER_MINOR 7
 #define OSAL_VER_PATCH 0
 
 #endif // OSAL_CONFIG_H
